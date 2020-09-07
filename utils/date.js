f2f58d8af3ae395f4e9fcef2bd1dcc45df01260e
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-09-07 13:08:03 +0530

    modified date and index fields

--- utils/date.js
+++ utils/date.js
@@ -3,6 +3,6 @@ let dd = String(today.getDate()).padStart(2, '0');
 let mm = String(today.getMonth() + 1).padStart(2, '0');
 let yyyy = today.getFullYear();
 
-today = mm + '-' + dd + '-' + yyyy;
+today =  yyyy + '.'+ mm + '.' + dd;
 
 module.exports = today;
--- utils/makeBulk.js
+++ utils/makeBulk.js
@@ -1,24 +1,45 @@
 const esConfig = require('../config/elasticsearch.json')
+// const fileName = require('../geomesa.json');
+// const indexName = 'wow';
 
 let bulk = [];
 
 //Creating bulk data to insert into Es
 const makeBulk = (file , indexName) => {
-    for(var current in file){
+
+    for(var current in file.features){
         bulk.push(
             { index: {_index : indexName , _type: esConfig.type , _id : current } },
                 {
-                    'version': file[current].version,
-                    'date': file[current].date,
-                    'npm': file[current].npm,
-                    'v8': file[current].v8,
-                    'uv': file[current].uv,
-                    'secure': file[current].security,         
+                    
+                    'account_no': file.features[current].properties.account_no,
+                    'ac_failure' : file.features[current].properties.ac_failure,
+  
+                    'date': file.features[current].properties.dtg,
+                    'dealer_id': file.features[current].properties.dealer_id,
+                    'device_id': file.features[current].properties.device_id,
+                    
+                    'id': file.features[current].id,
+                    'imei': file.features[current].properties.imei,
+                    
+                    'low_battery': file.features[current].properties.low_battery,
+                    'location': file.features[current].geometry.coordinates,
+
+                    'supervisory_loss' : file.features[current].properties.supervisory_loss,
+                    
+                    'tamper': file.features[current].properties.tamper,
+
                 }      
-            
+
             );
     }
     return bulk;
 } 
+
+
+
+
+// makeBulk(fileName , indexName);
+// console.log(bulk);
 
 module.exports = makeBulk;
